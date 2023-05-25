1e21580e310a862a6c4ade76e559d4dc0c2c2707
Kaj Käkelä <k-c-93@hotmail.com>
2023-05-26 00:24:14 +0300

    edits

--- day08/caesar_cipher.py
+++ day08/caesar_cipher.py
@@ -9,15 +9,14 @@ def main():
     choice = input("Type 'encode' to encrypt, 'decode' to decrypt:\n").lower()
 
     while choice != "encode" or choice != "decode":
-        if choice == "encode":
-            encode()
-        elif choice == "decode":
-            decode()
+        if choice == "encode" or choice == "decode":
+            encode_decode(choice)
         else:
-            print("Type 'encode' or 'decode'.")
+            choice = input("Type 'encode' to encrypt, 'decode' to decrypt:\n").lower()
+            
 
 
-def encode():
+def encode_decode(process):
     message = input("Type your message:\n").lower()
     shift_number = int(input("Type the shift number:\n"))
 
@@ -29,39 +28,11 @@ def encode():
     for letter in single_letter:
         if letter in alphabets:
             x = alphabets.index(letter)
-            new_message_list.append(alphabets[x+shift_number])
 
-        elif letter == " ":
-            new_message_list.append(" ")
-        
-        else:
-            continue
-    
-    
-    new_message = "".join(new_message_list)
-    print(f"Here's the encoded result: {new_message}")
-
-    choice = input("\nType 'yes' if you want to go again, otherwise type 'no':\n")
-
-    if choice == "yes":
-        main()
-    else:
-        exit()
-
-
-def decode():
-    message = input("Type your message:\n").lower()
-    shift_number = int(input("Type the shift number:\n"))
-
-    single_letter = [letter for letter in message]
-
-    new_message_list = []
-
-    
-    for letter in single_letter:
-        if letter in alphabets:
-            x = alphabets.index(letter)
-            new_message_list.append(alphabets[x-shift_number])
+            if process == "encode":
+                new_message_list.append(alphabets[x+shift_number])
+            else:
+                new_message_list.append(alphabets[x-shift_number])
 
         elif letter == " ":
             new_message_list.append(" ")
